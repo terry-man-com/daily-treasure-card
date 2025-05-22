8bbedb3eca6e1e5636fffe63846297a1f258f696
terry-man-com <terry.man.com@icloud.com>
2025-05-23 08:16:13 +0900

    create:home.blade.php,my-header.blade.php トップページ作成

---
+++ resources/views/components/my-header.blade.php
@@ -0,0 +1,7 @@
+<header class="bg-pink-300 shadow-md">
+    <div class="max-w-7xl pl-12 py-3 flex justify-between items-center">
+        <a href="{{ route('home')}}" class="text-[#007BFF] leading-relaxed font-bold" style="text-shadow: 1px 1px 0 beige, -1px -1px 0 beige, 1px -1px 0 beige, -1px 1px 0 beige;">
+            ワクワク宝集め
+        </a>
+    </div>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,11 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        {{-- Fonts GoogleFonts --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100..900&display=swap" rel="stylesheet">
+        
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
